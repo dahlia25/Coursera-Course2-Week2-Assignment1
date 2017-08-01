2b7686d3598ca9bf32c7f09577b4f014f4dd43f3
Dahlia Ma <ma.dahlia25@gmail.com>
2017-08-01 13:48:13 -0700

    Update Part 2 - complete.R

--- Part 2 - complete.R
+++ Part 2 - complete.R
@@ -1,1 +1,6 @@
+# Part 2:
+# Write a function that reads a directory full of files and reports the number of completely observed cases in each data file. 
+ 
+#The function should return a data frame where the first column is the name of the file and the second column is the number of 
+# complete cases.
 
